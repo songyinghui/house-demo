972b449428d9329f830f078cddfe1f480b935704
songyinghui <songyinghui>
2020-06-22 19:51:37 +0800

    6.20宋英辉作业之房屋出租系统

---
+++ config-server/src/main/java/com/syh/senior5/StartConfigServer.java
@@ -0,0 +1,13 @@
+package com.syh.senior5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class StartConfigServer {
+    public static void main(String[] args) {
+        SpringApplication.run(StartConfigServer.class,args);
+    }
+}
